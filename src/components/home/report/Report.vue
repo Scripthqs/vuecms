89b9c59d1f13656c4bf38e58384d2a25dc067c8d
Scripthqs <1941186772@qq.com>
2022-01-21 22:53:42 +0800

    2022-01-21 22:53:24

--- src/components/home/report/Report.vue
+++ src/components/home/report/Report.vue
@@ -3,16 +3,16 @@
     <Breadcrumb name1="数据统计" name2="数据报表" />
     <!--  卡片-->
     <el-card>
-      <!-- 2. 为ECharts准备一个具备大小（宽高）的Dom -->
+      <div class="card">
+        <!-- 2. 为ECharts准备一个具备大小（宽高）的Dom -->
+        <div id="main"></div>
 
-      <div id="main" style="width: 650px;height:300px;"></div>
+        <!-- 饼状图 -->
+        <div id="pie"></div>s
+        <!-- 柱状图 -->
+        <div id="bar"></div>
 
-      <!-- 地图模块 -->
-      <!-- <div id="bar" style="width: 650px;height:300px;">
-        <div></div>
-        <div></div>
-      </div> -->
-
+      </div>
     </el-card>
   </div>
 </template>
@@ -28,7 +28,7 @@ export default {
   data () {
     return {
       // 需要合并的数据
-      options1: {
+      options: {
         title: {
           text: '用户来源'
         },
@@ -58,84 +58,179 @@ export default {
             type: 'value'
           }
         ]
+
+      },
+      options2: {
+        title: {
+          text: '用户评价'
+        },
+        tooltip: {
+          trigger: 'axis'// 触发类型，可选值有item\axis
+          // triggerOn: 'click' // 触发时机，可选值mouseOver\click
+          // formatter: '{b}:{c}'// 格式化显示，字符串模板
+        },
+        toolbox: {
+          feature: { // 工具栏的按钮是配置在 feature 的节点之下
+            saveAsImage: {}, // 将图表保存为图片
+            dataView: {}, // 是否显示出原始数据
+            restore: {}, // 还原图表
+            // dataZoom: {}, // 数据缩放
+            magicType: { // 将图表在不同类型之间切换,图表的转换需要数据的支持
+              type: ['bar', 'line']
+            }
+          }
+        },
+        legend: {
+          data: ['质量', '服务', '物流']// 数组，data 的值需要和 series 数组中某组数据的 name 值一致
+        },
+        xAxis: {
+          type: 'category',
+          data: ['手机', '家电', '零食', '服装', '生活用品', '药品', '玩具', '珠宝']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            name: '物流',
+            type: 'bar',
+            data: [88, 92, 93, 77, 94, 80, 72, 86],
+            markPoint: {
+              data: [
+                {
+                  type: 'max', name: '最大值'
+                },
+                {
+                  type: 'min', name: '最小值'
+                }
+              ]
+            },
+            markLine: {
+              data: [
+                {
+                  type: 'average', name: '平均值'
+                }
+              ]
+            },
+            label: {
+              show: true, // 是否可见
+              rotate: 60 // 旋转角度
+            },
+            smooth: true,
+            lineStyle: { // 线条样式 lineStyle
+              color: 'green',
+              type: 'dashed' // 可选值还有 dotted solid
+            }
+            // barWidth: '50%' // 柱的宽度
+            // markArea: {
+            //   data: [
+            //     [
+            //       {
+            //         xAxis: '手机'
+            //       }, {
+            //         xAxis: '家电'
+            //       }
+            //     ]
+            //   ]
+            // }
+
+          },
+          {
+            name: '质量',
+            type: 'bar',
+            data: [88, 92, 83, 77, 84, 80, 72, 86],
+            markPoint: {
+              data: [
+                {
+                  type: 'max', name: '最大值'
+                },
+                {
+                  type: 'min', name: '最小值'
+                }
+              ]
+            },
+            markLine: {
+              data: [
+                {
+                  type: 'average', name: '平均值'
+                }
+              ]
+            },
+            label: {
+              show: true, // 是否可见
+              rotate: 60 // 旋转角度
+            }
+          },
+          {
+            name: '服务',
+            type: 'bar',
+            data: [93, 60, 61, 82, 95, 70, 71, 86],
+            markPoint: {
+              data: [
+                {
+                  type: 'max', name: '最大值'
+                },
+                {
+                  type: 'min', name: '最小值'
+                }
+              ]
+            },
+            markLine: {
+              data: [
+                {
+                  type: 'average', name: '平均值'
+                }
+              ]
+            },
+            label: {
+              show: true, // 是否可见
+              rotate: 60 // 旋转角度
+            }
+          }
+        ]
+
+      },
+      options3: {
+        title: {
+          text: '用户平台'
+        },
+        series: [
+          {
+            type: 'pie',
+            data: [
+              {
+                value: 11231,
+                name: '拼多多'
+              },
+              {
+                value: 22673,
+                name: '京东'
+              },
+              {
+                value: 6123,
+                name: '唯品会'
+              },
+              {
+                value: 8989,
+                name: '1号店'
+              },
+              {
+                value: 16700,
+                name: '淘宝'
+              }
+            ],
+            label: {
+              show: true,
+              formatter: function (arg) {
+                return arg.data.name + '平台' + arg.data.value + '元\n' +
+                  arg.percent + '%'
+              }
+            }
+            // roseType: 'radius'
+
+          }
+        ]
       }
-      // option2: {
-      //   color: ['#2f89cf'],
-      //   tooltip: {
-      //     trigger: 'axis',
-      //     axisPointer: {
-      //       // 坐标轴指示器，坐标轴触发有效
-      //       type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-      //     }
-      //   },
-      //   grid: {
-      //     left: '0%',
-      //     top: '10px',
-      //     right: '0%',
-      //     bottom: '4%',
-      //     containLabel: true
-      //   },
-      //   xAxis: [
-      //     {
-      //       type: 'category',
-      //       data: [
-      //         '旅游行业',
-      //         '教育培训',
-      //         '游戏行业',
-      //         '医疗行业',
-      //         '电商行业',
-      //         '社交行业',
-      //         '金融行业'
-      //       ],
-      //       axisTick: {
-      //         alignWithLabel: true
-      //       },
-      //       axisLabel: {
-      //         textStyle: {
-      //           color: 'rgba(255,255,255,.6)',
-      //           fontSize: '12'
-      //         }
-      //       },
-      //       axisLine: {
-      //         show: false
-      //       }
-      //     }
-      //   ],
-      //   yAxis: [
-      //     {
-      //       type: 'value',
-      //       axisLabel: {
-      //         textStyle: {
-      //           color: 'rgba(255,255,255,.6)',
-      //           fontSize: '12'
-      //         }
-      //       },
-      //       axisLine: {
-      //         lineStyle: {
-      //           color: 'rgba(255,255,255,.1)'
-      //           // width: 1,
-      //           // type: "solid"
-      //         }
-      //       },
-      //       splitLine: {
-      //         lineStyle: {
-      //           color: 'rgba(255,255,255,.1)'
-      //         }
-      //       }
-      //     }
-      //   ],
-      //   series: [
-      //     {
-      //       name: '直接访问',
-      //       type: 'bar',
-      //       barWidth: '35%',
-      //       data: [200, 300, 300, 900, 1500, 1200, 600],
-      //       itemStyle: {
-      //         barBorderRadius: 5
-      //       }
-      //     }
-      //   ]
-      // }
     }
   },
 
@@ -148,24 +243,36 @@ export default {
       return this.$message.error('获取折线图数据失败!')
     }
     // 4. 指定图表的配置项和数据
-    // console.log(res.data.xAxis[0].data)
-    const result = _.merge(res.data, this.options1)
+    console.log(res.data)
+    const result = _.merge(res.data, this.options)
+    console.log(result)
     // 5. 使用刚指定的配置项和数据显示图表。
     myChart1.setOption(result)
 
-    // 画图
-    // var myChart2 = echarts.init(document.getElementById('bar'))
-    // // const { data: res } = await this.$http.get('reports/type/1')
-    // // if (res.meta.status !== 200) {
-    // //   return this.$message.error('获取折线图数据失败!')
-    // // }
-    // // 4. 指定图表的配置项和数据
-    // // console.log(res.data.xAxis[0].data)
-    // // const result2 = _.merge(res.data, this.options2)
-    // myChart2.setOption(this.option2)
+    // 柱状图
+    var myChart2 = echarts.init(document.getElementById('bar'))
+    myChart2.setOption(this.options2)
+    //
+    var myChart3 = echarts.init(document.getElementById('pie'))
+    myChart3.setOption(this.options3)
   }
 }
 </script>
 
 <style lang='less' scoped>
+.card {
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+}
+#main,
+#pie {
+  width: 500px;
+  height: 300px;
+}
+#bar {
+  width: 1000px;
+  height: 300px;
+  margin-top: 60px;
+}
 </style>
